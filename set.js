7861f1725839de0773e3766abfd53dd473c4cf72
AleksBulajic <aleks.bulajic25@gmail.com>
2025-04-09 09:29:04 -0400

    set pseudocode

--- set.js
+++ set.js
@@ -1,71 +1,39 @@
 class Set {
-  constructor(list = []) {
+  constructor() {
     // Create a `values` property and set it equal to an empty array
-    this.values = [];
-    // Loop through the array and insert each item into the set
-    for (let item of list) {
-      this.insert(item);
-    }
+    // Accept a `list` parameter (default to an empty array). Loop
+    // through the array and insert each item into the set
   }
 
   length() {
-    // Return the length of the values property
-    return this.values.length;
+    // return the length of the values property
   }
 
   insert(val) {
-    // If `val` is not in the `values` property, then push it in
-    if (!this.has(val)) {
-      this.values.push(val);
-    }
+    // if `val` is not in the `values` property, then push it in
   }
 
   remove(val) {
-    // If `val` is in the `values` property, then remove it
-    if (this.has(val)) {
-      this.values = this.values.filter((item) => item !== val);
-    }
+    // if `val` is in the `values` property, then remove it
   }
 
   has(val) {
-    // Return true if `val` is in `values`, false if it isn't
-    return this.values.includes(val);
+    // return true if `val` is in `values`, false if it isn't
   }
 
   union(set) {
-    // Return a new Set with the values from this Set and the Set passed in as a parameter
-    const newSet = new Set(this.values);
-    for (let val of set.values) {
-      newSet.insert(val);
-    }
-    return newSet;
+    // return a new Set with the values from this Set and the
+    // Set passed in as a parameter
   }
 
   intersect(set) {
-    // Return a new Set of the values that appear in both this Set and the Set passed in
-    const newSet = new Set();
-    for (let val of this.values) {
-      if (set.has(val)) {
-        newSet.insert(val);
-      }
-    }
-    return newSet;
+    // return a new Set of the values that appear in both this
+    // Set and the Set passed in
   }
 
   difference(set) {
-    // Return a new Set of the values that only appear in one of the two sets
-    const newSet = new Set();
-    for (let val of this.values) {
-      if (!set.has(val)) {
-        newSet.insert(val);
-      }
-    }
-    for (let val of set.values) {
-      if (!this.has(val)) {
-        newSet.insert(val);
-      }
-    }
-    return newSet;
+    // return a new Set of the values that only appear in one of
+    // the two sets
   }
 }
 
